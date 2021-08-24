5b7755c46af26bb7319d51a8e2b72b8aa98d1c54
LukasNg <lukas.ng@ibm.com>
2021-08-24 22:33:40 +0100

    added published state & date functions

--- bwl_utils.py
+++ bwl_utils.py
@@ -55,5 +55,14 @@ def get_days_since_published(blueprint):
         dsp = 'N/A'
     return dsp
 
+def get_published_state(blueprint):
+    pub_state = blueprint['published-state']
+    return pub_state
 
+def get_published_date(blueprint):
+    pub_date = 'N/A'
+    pub_state = blueprint['published-state']
+    if pub_state == "published":
+        pub_date = blueprint['published-date']
+    return pub_date
 
