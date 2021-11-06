7acac409a0749c242909f848e983d8a6c1d93732
Gerry Baird <gerry.baird@uk.ibm.com>
2021-11-06 11:39:03 +0000

    Added archive script

--- blueprint-archive.py
+++ blueprint-archive.py
@@ -12,10 +12,7 @@ import argparse
 from tqdm import tqdm
 
 '''
-This script creates an extract of all blueprints in the account visible to the 
-service account you will have created in the bwl admin screens.
 
-Each blueprint is listed in a csv file showing various meta-data such as age.
 '''
 
 parser = argparse.ArgumentParser()
@@ -33,7 +30,7 @@ formatter = logging.Formatter('%(asctime)s %(levelname)-8s [%(filename)s:%(linen
 file_handler = logging.FileHandler('bwl-util.log')
 file_handler.setFormatter(formatter)
 logger.addHandler(file_handler)
-logger.setLevel(logging.INFO)
+logger.setLevel(logging.DEBUG)
 
 # Load the config
 try:
@@ -51,20 +48,25 @@ ROOT_URL = config['root-url']
 AUTH_URL = ROOT_URL + "/oauth/token"
 CLIENT_REPORTING_ID = config['artefact-reporting-client-id']
 CLIENT_REPORTING_SECRET = config['artefact-reporting-client-secret']
+CLIENT_AUTHORING_ID = config['artefact-authoring-client-id']
+CLIENT_AUTHORING_SECRET = config['artefact-authoring-client-secret']
 BLUEPRINT_LIB_URL = ROOT_URL + "/scr/api/LibraryArtifact?type=BLUEPRINT&returnFields=ID"
+SOURCE_SPACE_ID = config['source-space-id']
+BLUEPRINT_ARCHIVE_AGE_THRESHOLD = config['blueprint-archive-age-threshold']
 
+
+# Retrieve the reporting access token
 CLIENT_REPORTING_AUTH_DATA = {
     'grant_type': 'client_credentials',
     'client_id': CLIENT_REPORTING_ID,
     'client_secret': CLIENT_REPORTING_SECRET
 }
 
-# Get the access token here
 try:
     response = requests.post(AUTH_URL, data=CLIENT_REPORTING_AUTH_DATA)
     access_token = response.json()['access_token']
     if not access_token:
-        raise ValueError('Access token could not be retrieved, please check your input')
+        raise ValueError('Reporting access token could not be retrieved, please check your input')
 except ValueError as e:
     logger.warning(e)
     print(e)
@@ -72,22 +74,41 @@ except ValueError as e:
 
 CLIENT_REPORTING_ACCESS_TOKEN = access_token
 
+# Retrieve the authoring access token
+CLIENT_AUTHORING_AUTH_DATA = {
+    'grant_type': 'client_credentials',
+    'client_id': CLIENT_AUTHORING_ID,
+    'client_secret': CLIENT_AUTHORING_SECRET
+}
 
-async def get_blueprint_summaries(blueprint_list, bp_export, bp_errors):
-    connector = aiohttp.TCPConnector(limit=5)
+try:
+    response = requests.post(AUTH_URL, data=CLIENT_AUTHORING_AUTH_DATA)
+    auth_access_token = response.json()['access_token']
+    if not access_token:
+        raise ValueError('Authoring access token could not be retrieved, please check your input')
+except ValueError as e:
+    logger.warning(e)
+    print(e)
+    exit()
+
+CLIENT_AUTHORING_ACCESS_TOKEN = auth_access_token
+
+
+async def find_blueprints_for_archive(blueprint_list):
+    connector = aiohttp.TCPConnector(limit=1)
     async with aiohttp.ClientSession(connector=connector) as session:
         tasks = []
         pbar = tqdm(total=len(blueprint_list))
         for bp_id in blueprint_list:
             bp_id = bp_id.strip('/"')
-            task = asyncio.create_task(get_blueprint_data(session, bp_id, bp_export, bp_errors, pbar))
+            task = asyncio.create_task(check_blueprint_for_archive(session, bp_id, pbar))
             tasks.append(task)
 
         await asyncio.gather(*tasks)
         pbar.close()
 
 
-async def get_blueprint_data(session, bp_id, bp_export, bp_errors, pbar):
+async def check_blueprint_for_archive(session, bp_id, pbar):
     bp_url = ROOT_URL + "/bwl/blueprints/" + bp_id
 
     auth_value = f"Bearer {CLIENT_REPORTING_ACCESS_TOKEN}"
@@ -102,24 +123,22 @@ async def get_blueprint_data(session, bp_id, bp_export, bp_errors, pbar):
                 bp_json = await response.json()
                 bp_name = bwl_utils.get_name(bp_json)
                 space_name = bwl_utils.get_space_name(bp_json)
-                lmd = bwl_utils.get_last_modified_date(bp_json)
                 age = bwl_utils.get_age(bp_json)
+                archived_state = bp_json['archived-state']
 
-                bp_record = {'ID': bp_id, 'name': bp_name, 'space': space_name, 'last-modified': lmd, 'age': age}
-                bp_export.append(bp_record)
-
-                message = f"Finished processing blueprint ID : {bp_id}, Space : {space_name}, Name : {bp_name}"
-                logger.debug(message)
-
+                if age > BLUEPRINT_ARCHIVE_AGE_THRESHOLD and archived_state == "active":
+                    message = f"Archiving Blueprint ID : {bp_id}, Space : {space_name}, Name : {bp_name}, State : {archived_state}."
+                    logger.debug(message)
+                    archive_blueprint(bp_id)
+                else:
+                    message = f"Skipping Blueprint ID : {bp_id}, Space : {space_name}, Name : {bp_name}, State : {archived_state}."
+                    logger.debug(message)
             else:
                 message = f"Error processing blueprint : {bp_id}, response code from BWL : {status}"
                 logger.warning(message)
-                bp_error = {'ID': bp_id}
-                bp_errors.append(bp_error)
 
         except Exception as e:
-            bp_error = {'ID': bp_id}
-            bp_errors.append(bp_error)
+
             message = f"Unexpected error processing blueprint : {bp_id}"
             logger.error(message)
             logger.error(e)
@@ -127,14 +146,42 @@ async def get_blueprint_data(session, bp_id, bp_export, bp_errors, pbar):
         finally:
             pbar.update(1)
 
+def archive_blueprint(bp_id):
+
+    bp_archive_url = ROOT_URL + f"/bwl/artifacts/{bp_id}"
+    auth_value = f"Bearer {CLIENT_AUTHORING_ACCESS_TOKEN}"
+    headers = {
+        'Authorization': auth_value
+    }
+
+    params = {
+        'action': 'archive'
+    }
+
+    try:
+        archive_response = requests.put(bp_archive_url, headers=headers, params=params)
+        status = archive_response.status_code
+        if status == 200:
+            message = f"Archived Blueprint ID : {bp_id}"
+            logger.debug(message)
+        else:
+            message = f"Error archiving Blueprint ID : {bp_id}, status code {status}"
+            logger.debug(message)
+
+    except Exception as e:
+        logger.warning(e)
+        print(e)
+
+
 
 def get_blueprint_list():
     auth_value = f"Bearer {CLIENT_REPORTING_ACCESS_TOKEN}"
     head = {
         'Authorization': auth_value
     }
-
-    blueprint_lib_response = requests.get(BLUEPRINT_LIB_URL, headers=head).text
+    blueprint_by_space_url = ROOT_URL + ('/scr/api/LibraryArtifact?type=BLUEPRINT&returnFields=ID&spaceId='
+                                         + SOURCE_SPACE_ID)
+    blueprint_lib_response = requests.get(blueprint_by_space_url, headers=head).text
     blueprint_list = blueprint_lib_response.split('\n')
 
     # remove the first & last elements
@@ -146,7 +193,7 @@ def get_blueprint_list():
 
 def main():
     start_time = time.time()
-    msg = 'Starting BWL Summary Extract'
+    msg = 'Starting BWL Bulk Archive'
     print(msg)
     logger.info(msg)
 
@@ -155,25 +202,9 @@ def main():
     print(msg)
     logger.info(msg)
 
-    # Create lists for the output
-    bp_export = []
-    bp_errors = []
+    # create task list for blueprint archiv
+    asyncio.run(find_blueprints_for_archive(blueprint_list))
 
-    asyncio.run(get_blueprint_summaries(blueprint_list, bp_export, bp_errors))
-
-    # Save the data, use a context manager to handle the closure
-    with open('data_file.csv', 'w') as data_file:
-
-        # Standard headers
-        header = ['ID', 'Name', 'Space', 'LMD', 'Age in Days']
-        csv_writer = csv.writer(data_file)
-        row_count = 0
-        for bp_record in bp_export:
-            if row_count == 0:
-                csv_writer.writerow(header)
-                row_count += 1
-
-            csv_writer.writerow(bp_record.values())
 
     print("--- %s seconds ---" % (time.time() - start_time))
     logger.info('Finished')
